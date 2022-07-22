a9facc6afab88f0bc30d0ba1ac946ab5b2875ce7
Byron <byron.davison@gmail.com>
2022-07-21 22:14:43 -0700

    initial setup

--- script.js
+++
@@ -1,6 +0,0 @@
-
-const mongoose = require("mongoose")
-mongoose.connect("mongodb://localhost/testdb")
-const db =mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('connected to mongoose database'))
