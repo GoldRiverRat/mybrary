702a50432dea5fb73366328364ffc486b83889c4
Byron <byron.davison@gmail.com>
2022-08-04 22:34:26 -0700

    good

---
+++ routes/authors.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const router = express.Router()
+
+// all authors route
+router.get('/', (req, res) => {
+  res.render('authors/index')
+})
+
+// new author route
+router.get('/new', (req, res) => {
+  res.render('authors/new')
+})
+
+// create authors route
+router.post('/', (req, res) => {
+  res.send('create')
+})
+
+module.exports = router
